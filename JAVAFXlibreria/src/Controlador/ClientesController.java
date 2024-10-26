79befd0c4298882c7fb94e41ddadf7867ba2e7ec
JDavidTema <161101028+JDavidTema@users.noreply.github.com>
2024-10-25 21:02:08 -0600

    prueba
    a ver si da ajaja

--- JAVAFXlibreria/src/Controlador/ClientesController.java
+++ JAVAFXlibreria/src/Controlador/ClientesController.java
@@ -7,7 +7,11 @@ package Controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +19,29 @@ import javafx.fxml.Initializable;
  * @author 20041
  */
 public class ClientesController implements Initializable {
+
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtDireccion;
+    @FXML
+    private Button btnCancelar;
+    @FXML
+    private TableColumn<?, ?> nitPersona;
+    @FXML
+    private TableColumn<?, ?> nombreCliente;
+    @FXML
+    private TableColumn<?, ?> direccionCliente;
+    @FXML
+    private TableColumn<?, ?> telefonoCliente;
+    @FXML
+    private TextField txtTelefono;
+    @FXML
+    private Button btnModificar;
+    @FXML
+    private Button btnGuardar;
+    @FXML
+    private Button btnAnular;
 
     /**
      * Initializes the controller class.
--- JAVAFXlibreria/src/Modelo/ClientesTabla.java
+++ JAVAFXlibreria/src/Modelo/ClientesTabla.java
@@ -11,4 +11,68 @@ package Modelo;
  */
 public class ClientesTabla {
     
+    private Integer nitPersona;
+    private String nombreCliente;
+    private String direccionCliente;
+    private Integer telefonCliente;
+
+    /**
+     * @return the nitPersona
+     */
+    public Integer getNitPersona() {
+        return nitPersona;
+    }
+
+    /**
+     * @param nitPersona the nitPersona to set
+     */
+    public void setNitPersona(Integer nitPersona) {
+        this.nitPersona = nitPersona;
+    }
+
+    /**
+     * @return the nombreCliente
+     */
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    /**
+     * @param nombreCliente the nombreCliente to set
+     */
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    /**
+     * @return the direccionCliente
+     */
+    public String getDireccionCliente() {
+        return direccionCliente;
+    }
+
+    /**
+     * @param direccionCliente the direccionCliente to set
+     */
+    public void setDireccionCliente(String direccionCliente) {
+        this.direccionCliente = direccionCliente;
+    }
+
+    /**
+     * @return the telefonCliente
+     */
+    public Integer getTelefonCliente() {
+        return telefonCliente;
+    }
+
+    /**
+     * @param telefonCliente the telefonCliente to set
+     */
+    public void setTelefonCliente(Integer telefonCliente) {
+        this.telefonCliente = telefonCliente;
+    }
+    
 }
+
+
+
